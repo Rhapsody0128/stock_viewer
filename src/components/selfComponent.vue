c2e73d9d152db719aa6d8a28cc7cc6c68e713d20
Rhapsody0128 <rhapsody0128@gmail.com>
2021-08-11 10:36:23 +0800

    init

---
+++ src/components/selfComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button type="button" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
+</template>
+
+<script setup>
+import { defineProps, reactive } from "vue";
+
+defineProps({
+  msg: { type: String, default: "" },
+});
+
+const state = reactive({
+  count: 0,
+});
+</script>
+
+<script>
+// Vue2的寫法也可以
+
+// export default {
+//   props:{
+//     msg: {type:String,default:''}
+//   },
+//   data(){
+//     return {
+//       count:0
+//     }
+//   }
+// }
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    name: "Gary",
+    Age: 27,
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+});
